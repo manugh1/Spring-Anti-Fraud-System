aee7c7a59d14f1696a62f92b6309f093edb29b11
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-08-02 11:12:34 +0200

    Added Repository annotation to transaction
    also optimized the UserRepository and removed unused methods

--- src/main/java/io/github/dankoller/antifraud/persistence/TransactionRepository.java
+++ src/main/java/io/github/dankoller/antifraud/persistence/TransactionRepository.java
@@ -2,10 +2,13 @@ package io.github.dankoller.antifraud.persistence;
 
 import io.github.dankoller.antifraud.entity.transaction.Transaction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+// Repository for transaction entities
+@Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
     List<Transaction> findAllByDateBetweenAndNumber(LocalDateTime start, LocalDateTime end, String number);
 
--- src/main/java/io/github/dankoller/antifraud/persistence/UserRepository.java
+++ src/main/java/io/github/dankoller/antifraud/persistence/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+// Repository for user entities
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
@@ -14,6 +15,4 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
 
     List<User> findAll();
-
-    boolean existsByUsername(String username);
 }
