5bd164f86909deda774c3e45a63addc3fd342473
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-09-10 12:51:08 +0200

    Added suspicious ip address management to  support actions

--- src/main/java/io/github/dankoller/antifraud/controller/WebController.java
+++ src/main/java/io/github/dankoller/antifraud/controller/WebController.java
@@ -1,7 +1,10 @@
 package io.github.dankoller.antifraud.controller;
 
+import io.github.dankoller.antifraud.entity.IPAddress;
+import io.github.dankoller.antifraud.entity.transaction.Transaction;
 import io.github.dankoller.antifraud.response.UserDataResponse;
 import io.github.dankoller.antifraud.service.UserService;
+import io.github.dankoller.antifraud.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +20,9 @@ public class WebController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ValidationService validationService;
 
     // Using request mapping instead of get mapping because of the redirects in the html
 
@@ -41,15 +47,44 @@ public class WebController {
         return "merchant";
     }
 
-    @RequestMapping("/merchant/transaction")
+    @RequestMapping("/merchant/new-transaction")
     public String getMerchantTransactionPage() {
-        return "/merchant/transaction";
+        return "/merchant/new-transaction";
     }
 
     // Support view related endpoints
     @RequestMapping("/support")
     public String getSupportPage() {
         return "support";
+    }
+
+    @RequestMapping("/support/list-users")
+    public String getListPage(Model model) {
+        List<UserDataResponse> userDataResponses = userService.getUserDataList();
+        model.addAttribute("users", userDataResponses);
+        return "/support/list-users";
+    }
+
+    @RequestMapping("/support/manage-ip")
+    public String getManageIpPage(Model model) {
+        List<IPAddress> suspiciousIPs = (List<IPAddress>) validationService.getSuspiciousIPs();
+        model.addAttribute("ips", suspiciousIPs);
+        return "/support/manage-ip";
+    }
+
+    @RequestMapping("/support/manage-stolencard")
+    public String getManageStolenCardPage() {
+        return "/support/manage-stolencard";
+    }
+
+    @RequestMapping("/support/review-transaction")
+    public String getReviewTransactionPage() {
+        return "/support/review-transaction";
+    }
+
+    @RequestMapping("/support/transaction-history")
+    public String getTransactionHistoryPage() {
+        return "/support/transaction-history";
     }
 
     // Admin view related endpoints
@@ -66,13 +101,6 @@ public class WebController {
     @RequestMapping("/admin/delete-user")
     public String getDeleteUserPage() {
         return "/admin/delete-user";
-    }
-
-    @RequestMapping("/support/list-users")
-    public String getListPage(Model model) {
-        List<UserDataResponse> userDataResponses = userService.getUserDataList();
-        model.addAttribute("users", userDataResponses);
-        return "/support/list-users";
     }
 
     @RequestMapping("/admin/update-role")
