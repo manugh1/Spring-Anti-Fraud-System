58fb48aaf07cb8fbe92a52f1af15af8628440fbe
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-09-21 14:07:54 +0200

    Added a custom error page for handling 403, 404 and 500 specifically
    All other errors are caught on the same page but with a default phrase

---
+++ src/main/java/io/github/dankoller/antifraud/controller/WebErrorController.java
@@ -0,0 +1,33 @@
+package io.github.dankoller.antifraud.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class WebErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            // Add status code to model
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                model.addAttribute("error", "404");
+            } else if(statusCode == HttpStatus.FORBIDDEN.value()) {
+                model.addAttribute("error", "403");
+            } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                model.addAttribute("error", "500");
+            }
+        }
+        return "/error/error";
+    }
+}
