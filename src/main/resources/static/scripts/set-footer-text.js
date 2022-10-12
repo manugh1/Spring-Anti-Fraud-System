36bfa73ada78a4a2dd29a06593826365330025e1
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-10-12 09:48:01 +0200

    Removed Thymeleaf due to new architecture (frontend is now rendered client-side)
    The old ssr frontend is available as separate package that you can download and add to the api yourself. Instructions will be provided. However, the thymeleaf frontend is no longer maintained from now on.

--- src/main/resources/static/scripts/set-footer-text.js
+++
@@ -1,6 +0,0 @@
-// Updates the copyright footer
-const today = new Date();
-const year = today.getFullYear();
-
-let elem = document.getElementById("copyright-footer");
-elem.innerHTML = "©" + year + ' made with <i class="fa fa-heart" style="color:red"></i> by - <a target="_blank" rel="noopener noreferrer" href="https://dan-koller.github.io/"> Dan Koller</a>';
--- src/main/resources/static/scripts/sidebar-count-tasks.js
+++
@@ -1,4 +0,0 @@
-// Count all the <li> elements in the dropdown menu
-var count = document.getElementById("tasks-menu").getElementsByTagName("li").length;
-// Set the count in the badge
-document.getElementById("tasks-count").innerHTML = count;
